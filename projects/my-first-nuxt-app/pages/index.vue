dc03a555e99f90c61ebece653ac6f7c4f3483b8e
Fabrizio Pace <pacefabriziowork@gmail.com>
2021-11-29 20:08:53 +0100

    Creating the Landing Page

--- projects/my-first-nuxt-app/pages/index.vue
+++ projects/my-first-nuxt-app/pages/index.vue
@@ -1,5 +1,110 @@
 <template>
-  <section class="container">
-    <nuxt-link to="/users">Users</nuxt-link>
-  </section>
+  <div class="home-page">
+    <section class="intro">
+      <h1>Get the latest tech news!</h1>
+    </section>
+    <section class="featured-posts">
+      <nuxt-link :to="'/posts/' + 1">
+        <article class="post-preview">
+          <div class="post-thumbnail" style="background-image:url('https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80')">
+            <div class="post-content">
+              <h1>Post Title</h1>
+              <p>Preview Text</p>
+            </div>
+          </div>
+        </article></nuxt-link
+      >
+      <nuxt-link :to="'/posts/' + 2">
+        <article class="post-preview">
+          <div class="post-thumbnail" style="background-image:url('https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80')">
+            <div class="post-content">
+              <h1>Post Title 2</h1>
+              <p>Preview Text</p>
+            </div>
+          </div>
+        </article></nuxt-link
+      >
+    </section>
+  </div>
 </template>
+
+<style scoped>
+.intro {
+  height: 300px;
+  position: relative;
+  padding: 30px;
+  box-sizing: border-box;
+  background-position: center;
+  background-size: cover;
+}
+
+.intro h1 {
+  position: absolute;
+  top: 10%;
+  left: 5%;
+  width: 90%;
+  font-size: 1.5rem;
+  color: black;
+  background-color: rgb(211, 211, 211);
+  padding: 10px;
+  border-radius: 10px;
+  box-shadow: 3px 3px 3px black;
+  box-sizing: border-box;
+  border: 1px solid black;
+}
+
+@media (min-width: 768px) {
+  .intro h1 {
+    font-size: 2rem;
+  }
+}
+
+.featured-posts {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+
+.post-preview {
+  border: 1px solid #ccc;
+  box-shadow: 0 2px 2px #ccc;
+  background-color: white;
+  width: 90%;
+}
+
+.post-content {
+    background: #cccccca8;
+}
+
+a {
+  text-decoration: none;
+  color: black;
+}
+
+@media (min-width: 850px) {
+  .post-preview {
+    width: 400px;
+    margin: 10px;
+  }
+}
+
+.post-thumbnail {
+  width: 100%;
+  height: 200px;
+  background-position: center;
+  background-size: cover;
+}
+
+.post-content {
+  padding: 10px;
+  text-align: center;
+}
+
+a:hover .post-content,
+a:active .post-content {
+  background-color: #ccc;
+}
+</style>
