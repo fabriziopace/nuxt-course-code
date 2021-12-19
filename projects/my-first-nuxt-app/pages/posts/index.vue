944b00f96c6b11f9ba2bfb8abc264be025ad181c
Fabrizio Pace <pacefabriziowork@gmail.com>
2021-12-19 21:28:39 +0100

    Vuex, fetch() and nuxtServerInit()

--- projects/my-first-nuxt-app/pages/posts/index.vue
+++ projects/my-first-nuxt-app/pages/posts/index.vue
@@ -19,44 +19,10 @@ export default {
   components: {
     PostList,
   },
-  asyncData(context) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve({
-          loadedPosts: [
-            {
-              id: "1",
-              title: "First Post",
-              previewText: "This is our first post!",
-              thumbnail:
-                "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
-            },
-            {
-              id: "2",
-              title: "Second Post",
-              previewText: "This is our second post!",
-              thumbnail:
-                "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
-            },
-          ],
-        });
-      }, 1500);
-    })
-      .then((data) => {
-        return data;
-      })
-      .catch((e) => {
-        context.error(new Error());
-      });
-  },
-  // data() {
-  //   return {
-  //     loadedPosts: [],
-  //   };
-  // },
-  created() {
-    this.$store.dispatch("setPosts", this.loadedPosts);
-    console.log(this.$store.getters.loadedPosts);
+  computed: {
+    loadedPosts() {
+      return this.$store.getters.loadedPosts;
+    },
   },
 };
 </script>
--- projects/my-first-nuxt-app/store/index.js
+++ projects/my-first-nuxt-app/store/index.js
@@ -11,6 +11,29 @@ const createStore = () => {
             }
         },
         actions: {
+            nuxtServerInit(vuexContext, context) {
+                return new Promise((resolve, reject) => {
+                    setTimeout(() => {
+                        vuexContext.commit('setPosts', [
+                            {
+                                id: "1",
+                                title: "First Post",
+                                previewText: "This is our first post!",
+                                thumbnail:
+                                    "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+                            },
+                            {
+                                id: "2",
+                                title: "Second Post",
+                                previewText: "This is our second post!",
+                                thumbnail:
+                                    "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+                            },
+                        ]);
+                        resolve();
+                    }, 1500);
+                });
+            },
             setPosts(vuexContext, posts) {
                 vuexContext.commit('setPosts', posts);
             }
