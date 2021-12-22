8e700202307ceb7468a1d34b773d48ec05714255
Fabrizio Pace <pacefabriziowork@gmail.com>
2021-12-22 23:42:55 +0100

    Fetching Data from the Backend

--- projects/my-first-nuxt-app/store/index.js
+++ projects/my-first-nuxt-app/store/index.js
@@ -1,4 +1,5 @@
 import Vuex from 'vuex';
+import axios from 'axios';
 
 const createStore = () => {
     return new Vuex.Store({
@@ -12,30 +13,15 @@ const createStore = () => {
         },
         actions: {
             nuxtServerInit(vuexContext, context) {
-                if (!process.client) {
-                    console.log(context.req.session);
-                }
-                return new Promise((resolve, reject) => {
-                    setTimeout(() => {
-                        vuexContext.commit('setPosts', [
-                            {
-                                id: "1",
-                                title: "First Post",
-                                previewText: "This is our first post!",
-                                thumbnail:
-                                    "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
-                            },
-                            {
-                                id: "2",
-                                title: "Second Post",
-                                previewText: "This is our second post!",
-                                thumbnail:
-                                    "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
-                            },
-                        ]);
-                        resolve();
-                    }, 1500);
-                });
+                return axios.get('firebaseurl')
+                    .then(res => {
+                        const postsArray = []
+                        for (const key in res.data) {
+                            postsArray.push({ ...res.data[key], id: key });
+                        }
+                        vuexContext.commit('setPosts', postsArray);
+                    })
+                    .catch(e => context.error(e));
             },
             setPosts(vuexContext, posts) {
                 vuexContext.commit('setPosts', posts);
