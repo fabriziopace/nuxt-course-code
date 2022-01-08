f047ec22b3926529b78386e8f35201036b04b32f
Fabrizio Pace <pacefabriziowork@gmail.com>
2022-01-08 22:14:25 +0100

    Adding Plugins

--- projects/my-first-nuxt-app/pages/posts/index.vue
+++ projects/my-first-nuxt-app/pages/posts/index.vue
@@ -13,12 +13,7 @@
 </style>
 
 <script>
-import PostList from "@/components/Posts/PostList";
-
 export default {
-  components: {
-    PostList,
-  },
   computed: {
     loadedPosts() {
       return this.$store.getters.loadedPosts;
